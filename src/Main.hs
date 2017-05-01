633f89d4f19e013c37ebe9118a62ac9d90ab04f2
Alexey Ozeritsky <aozeritsky@yandex-team.ru>
2017-05-01 17:36:30 +0300

    prototype for spider

--- src/Main.hs
+++ src/Main.hs
@@ -4,12 +4,59 @@ module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import           Network.HTTP.Simple
+import Control.Monad
+import Control.Concurrent
+import Control.Concurrent.BoundedChan
+import Control.Concurrent.Chan
+
+data HttpTask = HttpTask {
+  uri :: String,
+  startTime:: Int } | HttpStop deriving (Show)
+
+newtype HttpQueue = HttpQueue (MVar HttpTask)
+
+newHttpQueue :: IO HttpQueue
+newHttpQueue = do
+  m <- newEmptyMVar
+  let h = HttpQueue m
+  return h
+
+{- scheme:
+uri -> executor -> [[ doanloaders ]] -> [[parsers]] ->
+         ^                                           |
+         |                                           V
+         ^-------------------------------------------<
+
+executor -> Control.Concurrent.BoundedChan -> [[ downloaders ]]
+[[parsers]] -> Control.Concurrent.Chan -> executor
+-}
+
+{-
+ TODO: rename -> executor
+ -}
+download :: HttpQueue -> IO ()
+download (HttpQueue x) = loop
+  where
+    loop = do
+      cmd <- takeMVar x
+      case cmd of
+        HttpTask { uri = u, startTime = _ } -> do
+          putStrLn u
+          loop
+        HttpStop -> return ()
 
 main :: IO ()
 main = do
-    response <- httpLBS "http://httpbin.org/get"
+    let reqs = ["http://httpbin.org/get", "http://httpbin.org/get", "http://google.com"]
+    queue <- newHttpQueue
+    return ()
 
-    putStrLn $ "The status code was: " ++
-               show (getResponseStatusCode response)
-    print $ getResponseHeader "Content-Type" response
-    L8.putStrLn $ getResponseBody response
+    {-
+    responses <- mapM httpLBS reqs
+
+    forM_ responses (\response -> do
+      putStrLn $ "The status code was: " ++ show (getResponseStatusCode response)
+      print $ getResponseHeader "Content-Type" response
+      L8.putStrLn $ getResponseBody response)
+    return ()
+-}
