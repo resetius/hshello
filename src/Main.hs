eb697b89c20487d9c66b25249671a52dfc793e77
Alexey Ozeritsky <aozeritsky@yandex-team.ru>
2017-05-02 14:25:23 +0300

    spider can walk links

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,8 @@ import Control.Monad
 import Control.Concurrent
 import qualified Control.Concurrent.BoundedChan as BChan
 import Control.Concurrent.Chan
+import Text.HTML.TagSoup
+import Text.StringLike
 
 data Message = DownloaderRequest {
   uri :: String
@@ -29,6 +31,31 @@ executor -> Control.Concurrent.BoundedChan -> [[ downloaders ]]
 [[parsers]] -> Control.Concurrent.Chan -> executor
 -}
 
+tagFilter :: (StringLike a) => Tag a -> Bool
+tagFilter (TagOpen "a" attrs) =
+  let r = lookup "href" attrs in
+    case r of
+      Nothing -> False
+      _ -> True
+tagFilter tag = False
+
+tagHref :: (StringLike a) => Tag a -> String
+tagHref (TagOpen "a" attrs) =
+  let r = lookup "href" attrs in
+    case r of
+      Just a -> toString a
+      _ -> "_"
+tagHref tag = "_"
+
+extractLinks :: (StringLike body) => Response body -> [String]
+extractLinks response =
+  let tags = parseTags $ getResponseBody response in
+    map tagHref $ filter tagFilter tags
+
+enrichLink :: String -> String -> String
+enrichLink _ link@('h':'t':'t':'p':_) = link
+enrichLink url link = url ++ link
+
 executor :: HttpQueue -> BoundedHttpQueue -> MVar Message -> IO ()
 executor input output ctl = loop
   where
@@ -42,30 +69,36 @@ executor input output ctl = loop
           putMVar ctl cmd
           return ()
 
-downloader :: BoundedHttpQueue -> IO()
-downloader input = loop
+downloader :: BoundedHttpQueue -> HttpQueue -> IO()
+downloader input output = loop
   where
     loop = do
       cmd <- BChan.readChan input
       case cmd of
         DownloaderRequest { uri = u } -> do
+          putStrLn $ "Downloading " ++ u
           request <- parseRequest u
           response <- httpLBS request
           putStrLn $ "The status code of " ++  u ++ " was " ++ show (getResponseStatusCode response)
+          {-let tags = parseTags $ getResponseBody response
+          forM_ tags print-}
+          let links = map (enrichLink u) $ extractLinks response
+          {-forM_ links print-}
+          forM_ links $ writeChan output . DownloaderRequest
           loop
         Stop -> return ()
 
 main :: IO ()
 main = do
-    let reqs = ["http://httpbin.org/get", "http://httpbin.org/get", "http://google.com"]
+    let reqs = ["http://httpbin.org"]
     let ndownloaders = 1000
     executorInput <- newChan
     executorOutput <- BChan.newBoundedChan ndownloaders
     ctl <- newEmptyMVar
-    forM_ [1 .. ndownloaders] (\_ -> forkIO $ downloader executorOutput)
+    forM_ [1 .. ndownloaders] (\_ -> forkIO $ downloader executorOutput executorInput)
     forkIO $ executor executorInput executorOutput ctl
     {-takeMVar ctl-}
-    forM_ reqs (\r -> writeChan executorInput $ DownloaderRequest r)
+    forM_ reqs $ writeChan executorInput . DownloaderRequest
     threadDelay 100000000
     writeChan executorInput Stop
     {-executor executorInput executorOutput ctl-}
